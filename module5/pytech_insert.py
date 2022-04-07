e68ac34cc7d9df3e2697b7fcde8dd4ddc6558fb3
Kyle Sullivan <rocket_kyle@yahoo.com>
2022-04-07 17:41:52 -0600

    Created the pytech_insert.py and pytech_queries.py files

---
+++ module5/pytech_insert.py
@@ -0,0 +1,48 @@
+# Import the Mongo DB package
+from pymongo import MongoClient
+
+# Create the url for the db connection
+url = "mongodb+srv://admin:admin@cluster0.dkpqp.mongodb.net/pytech"
+
+# Create the db connection object
+client = MongoClient(url)
+
+# Connect to the DB
+db = client.pytech
+
+""" three student documents"""
+# Darth Vader's data document 
+vader = {
+    "student_id": "1007",
+    "first_name": "Darth",
+    "last_name": "Vader"
+}
+
+# Obiwan Kenobi data document 
+obiwan = {
+    "student_id": "1008",
+    "first_name": "Obiwan",
+    "last_name": "Kenobi"
+}
+
+# Mace Windu data document
+mace = {
+    "student_id": "1009",
+    "first_name": "Mace",
+    "last_name": "Windu"
+}
+
+students = db.students
+
+# insert statements with output 
+print("\n  -- INSERT STATEMENTS --")
+vader_student_id = students.insert_one(vader).inserted_id
+print("Inserted student record Darth Vader into the students collection with document_id " + str(vader_student_id))
+
+obiwan_student_id = students.insert_one(obiwan).inserted_id
+print("Inserted student record Obiwan Kenobi into the students collection with document_id " + str(obiwan_student_id))
+
+mace_student_id = students.insert_one(mace).inserted_id
+print("Inserted student record Mace Windu into the students collection with document_id " + str(mace_student_id))
+
+input("\n\n  End of program, press any key to exit... ")
---
+++ module5/pytech_queries.py
@@ -0,0 +1,29 @@
+# Import the Mongo DB package
+from pymongo import MongoClient
+
+# Create the url for the db connection
+url = "mongodb+srv://admin:admin@cluster0.dkpqp.mongodb.net/pytech"
+
+# Create the db connection object
+client = MongoClient(url)
+
+# Connect to the DB
+db = client.pytech
+
+students = db.students
+
+student_list = students.find({})
+
+print("\n -- DISPLAYING STUDENT DOCUMENTS FROM .find() THE QUERY --")
+
+for doc in student_list:
+    print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
+
+obiwan = students.find_one({"student_id": "1008"})
+
+# output the results 
+print("\n  -- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
+print("  Student ID: " + obiwan["student_id"] + "\n  First Name: " + obiwan["first_name"] + "\n  Last Name: " + obiwan["last_name"] + "\n")
+
+# exit message 
+input("\n\n  End of program, press any key to continue...")
